7930e4de379c03aedaee963dfe24c41b466318a6
s1nstyle <joonpark0419@hotmail.com>
2022-11-30 05:08:07 -0500

    brought back serverside, changed ip

--- project/pages/index.tsx
+++ project/pages/index.tsx
@@ -17,14 +17,14 @@ export default Home;
 
 //Used ServerSideProps since further down the road when I need to update my skillset, I will
 //be able to do that in the backend and the changes will reflect onto the page
-// export async function getServerSideProps() {
-//   const response = await axios.get(`http://s1n.vercel.app/api/getTech`);
-//   let techStackLst = []
-//   if (response.status === 200) {
-//     const data = response.data['techStack']
-//     for (let i = 0; i < data.length; i++) {
-//       techStackLst.push(data[i]['stack'])
-//     }
-//   }
-//   return { props: {techStackLst}}
-// }
+export async function getServerSideProps() {
+  const response = await axios.get(`http://s1n.vercel.app/api/getTech`);
+  let techStackLst = []
+  if (response.status === 200) {
+    const data = response.data['techStack']
+    for (let i = 0; i < data.length; i++) {
+      techStackLst.push(data[i]['stack'])
+    }
+  }
+  return { props: {techStackLst}}
+}
