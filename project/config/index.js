84c9d28e9ef4bded6a0cedf3e0c48c589fccb56d
s1nstyle <joonpark0419@hotmail.com>
2022-11-30 02:38:55 -0500

    removed typo

--- project/config/index.js
+++ project/config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? 'http://s1n.vercel.app/' : 'http://localhost:3000'
+export const server = dev ? 'http://s1n.vercel.app' : 'http://localhost:3000'
